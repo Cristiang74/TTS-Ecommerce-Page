03a0a182a822201a73f1a6e0b232860206fa50bd
Cristian Garcia <cg7497@gmail.com>
2020-11-09 07:41:29 -0600

    Working page with items and pay, no cart

---
+++ src/main/java/com/tts/ecommercepage/model/Product.java
@@ -0,0 +1,110 @@
+package com.tts.ecommercepage.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private int quantity;
+    private Long price;
+    private String description;
+    private String name;
+    private String brand;
+    private String category;
+    private String image;
+
+    public Product() {
+    }
+
+    public Product(Long id, int quantity, Long price, String description, String name, String brand, String category,
+            String image) {
+        this.id = id;
+        this.quantity = quantity;
+        this.price = price;
+        this.description = description;
+        this.name = name;
+        this.brand = brand;
+        this.category = category;
+        this.image = image;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public Long getPrice() {
+        return price;
+    }
+
+    public void setPrice(Long price) {
+        this.price = price;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public void setBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [brand=" + brand + ", category=" + category + ", description=" + description + ", id=" + id
+                + ", image=" + image + ", name=" + name + ", price=" + price + ", quantity=" + quantity + "]";
+    }
+
+   
+
+
+}
